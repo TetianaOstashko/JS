3bf4433cc45f08cb10b01c42dd6b4c54436831c1
TetianaOstashko <tetianaostashko.25@gmail.com>
2023-02-23 23:13:12 +0100

    Script to shows sum, deduction, multiplication and division results

---
+++ script.js
@@ -0,0 +1,12 @@
+let firstNumber = prompt('Please enter first number');
+let secondNumber = prompt('Please enter second number');
+
+let sum = Number(firstNumber) + Number(secondNumber);
+let deduction = firstNumber - secondNumber;
+let multiplication = firstNumber * secondNumber;
+let division = firstNumber / secondNumber;
+
+
+alert ('Sum:' + sum + '\n' + 'Deduction: ' + deduction + '\n' + 'Multiplication: ' + multiplication + '\n' + 'Division: ' + division);
+
+
