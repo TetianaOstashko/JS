43251ce1b280ddebc2f6d5bc4e244c64eb84da16
TetianaOstashko <tetianaostashko.25@gmail.com>
2023-04-08 16:36:41 +0200

    The source code has been fixed

--- errors.js
+++ errors.js
@@ -7,22 +7,35 @@ function calculateAverage(arr) {
     
     let avg = sum / arr.length;
     return avg;
-  }
+}
   
-  let numbers = [1, 2, 3, 4, 5];
+let numbers = [1, 2, 3, 4, 5];
   
-  let avg = calculateAverage(numbers);
-  console.log("The average is: " + avg);
+let avg = calculateAverage(numbers);
+console.log("The average is: " + avg);
 
-  //2
-  function User(name, age) {
+
+//2
+function User(name, age) {
     this.name = name;
     this.age = age;
-  }
+}
   
-  User.prototype.sayHello = function() {
+User.prototype.sayHello = function() {
     return `Hi. My name is ${this.name} and I'm ${this.age} years old`;
-  }
+}
 
-  const user = new User('Sherlock', 27);
-  console.log(user.sayHello());
+const user = new User('Sherlock', 27);
+console.log(user.sayHello());
+
+
+//3
+const calculator = function(init = 0) {
+    return function () {
+  	    return init++;
+    }
+}
+
+const c = calculator();
+console.log(c()); 
+console.log(c());
