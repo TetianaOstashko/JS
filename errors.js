6510ab3fe4aa8faacf217d0b12f134d0686e327e
TetianaOstashko <tetianaostashko.25@gmail.com>
2023-04-08 16:34:06 +0200

    The source code has been fixed

---
+++ errors.js
@@ -0,0 +1,28 @@
+// 1
+function calculateAverage(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+      sum += arr[i];
+    }
+    
+    let avg = sum / arr.length;
+    return avg;
+  }
+  
+  let numbers = [1, 2, 3, 4, 5];
+  
+  let avg = calculateAverage(numbers);
+  console.log("The average is: " + avg);
+
+  //2
+  function User(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  
+  User.prototype.sayHello = function() {
+    return `Hi. My name is ${this.name} and I'm ${this.age} years old`;
+  }
+
+  const user = new User('Sherlock', 27);
+  console.log(user.sayHello());
