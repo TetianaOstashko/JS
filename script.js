8cab7dffb2e7f6d9ee0aa3697a98ea7551c75acb
TetianaOstashko <tetianaostashko.25@gmail.com>
2023-04-22 18:08:53 +0200

    Methods pow and defer are added

--- script.js
+++ script.js
@@ -1,97 +1,27 @@
-const num1 = parseFloat(prompt("Enter the first number:"));
-const num2 = parseFloat(prompt("Enter the second number:"));
-
-// function to check if the value is number or not
-function checkIfNumber(enteredValue, message) {
-  if (isNaN(enteredValue)) {
-    const errorMsg = message ? message : "Please, enter a number";
-    document.getElementById("result").innerHTML = errorMsg;
-    return false;
+Array.prototype.pow = function(n) {
+  const result = [];
+  for (let i = 0; i < this.length; i++)  {
+    result.push(Math.pow(this[i], n));
   }
-  return true;
+  return result;
 }
 
-// function to make gradient
-function gradient(result, resultElement) {
-  if (result > 5000 && result <= 100000) {
-      resultElement.style.color = 'rgb(222, 122, 252)';
-  } else if (result > 1000 && result <= 5000) {
-      resultElement.style.color = 'rgb(233, 95, 125)';
-    } else if (result > 100 && result <= 1000) {
-      resultElement.style.color = 'rgb(245, 55, 75)';
-  } else  if (result > 0 && result <= 100) {
-     resultElement.style.color = 'rgb(255, 0, 0)';
+const arr = [2, 12, 25];
+const arrPow = arr.pow(2);
 
-  } else if (result <= 0 && result >= -100) {
-      resultElement.style.color = 'rgb(206, 48, 55)';
-  } else if (result < -100 && result >= -1000) {
-      resultElement.style.color = 'rgb(228, 76, 22)';
-  } else if (result < -1000 && result >= -50000) {
-      resultElement.style.color = 'rgb(77, 15, 10)';
-  } else if (result < -50000 && result >= -1000000) {
-      resultElement.style.color = 'rgb(41, 3, 3)';
-  }
+console.log(arrPow);
+
+
+function check() {
+  alert("It works!");
 }
 
-//function to add color
-function makeResultsColored(result) {
-  const resultElement = document.getElementById("result");
-  gradient(result, resultElement);
-  resultElement.innerHTML = `Result is: ${result}`;
+Function.prototype.defer = function (n) {
+  let fnctn = this;
+
+  setTimeout(function() {
+    fnctn();
+  }, n)
 }
 
-function sum() {
-  if (checkIfNumber(num1) && checkIfNumber(num2)) {
-    const result = num1 + num2;
-    makeResultsColored(result);
-  }
-}
- 
-function subtraction() {
-  if (checkIfNumber(num1) && checkIfNumber(num2)) {
-    const result = num1 - num2;
-    makeResultsColored(result);
-  }
-}
-  
-function multiplication() {
-  if (checkIfNumber(num1) && checkIfNumber(num2)) {
-    const result = num1 * num2;
-    makeResultsColored(result);
-  }
-}
-  
-function division() {
-  if (checkIfNumber(num1) && checkIfNumber(num2)) {
-    if (num2 === 0) {
-      document.getElementById("result").innerHTML = "ERROR, there's no way to divide a number by 0";
-      document.getElementById("result").style.color = "red";
-    } else {
-      const result = num1 / num2;
-      makeResultsColored(result);
-    }
-  }
-}
-
-// function to find Min & Max elements in array
-const array = [233, 6, -9, 0, -252, 1025, 999, 125];
-function findMin(array) {
-  let min = array[0];
-  for (let i = 1; i < array.length; i++) {
-    if (array[i] < min) {
-      min = array[i];
-    }
-  }
-  document.getElementById("resultMinMax").innerHTML = "Min element is: " + min;
-}
-
-function findMax(array) {
-  let max = array[0];
-  for (let i = 1; i < array.length; i++) {
-    if (array[i] > max) {
-      max = array[i];
-    }
-  }
-  document.getElementById("resultMinMax").innerHTML = "Max element is: " + max;
-}
-
+check.defer(2000);
